fc8c1ce46063fd1bc0ea2841e0bec73edc65e012
John Sandsjö <john.sandsjo@gmail.com>
2025-01-26 23:16:10 +0100

    video lecture 08

---
+++ video_lectures/08_consumer_producer/src/consumer.py
@@ -0,0 +1,25 @@
+from quixstreams import Application
+
+app = Application(
+    broker_address="localhost:9092",
+    consumer_group="text-splitter",
+    auto_offset_reset="earliest",
+)
+
+jokes_topic = app.topic(name="jokes", value_deserializer="json")
+
+
+sdf = app.dataframe(topic=jokes_topic)
+
+
+sdf = sdf.update(lambda message: print(f"Input: {message}"))
+
+sdf = sdf.apply(lambda message: [{"word": word} for word in message["joke_text"].split()], expand=True)
+
+sdf["lenght"] = sdf["word"].apply(lambda word: len(word))
+
+sdf = sdf.update(lambda row: print(f"Output: {row}"))
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ video_lectures/08_consumer_producer/src/producer.py
@@ -0,0 +1,34 @@
+from quixstreams import Application
+from pathlib import Path
+import json
+from pprint import pprint
+
+app = Application(broker_address="localhost:9092", consumer_group="text-splitter")
+
+jokes_topic = app.topic(name="jokes", value_serializer="json")
+
+jokes_path = Path(__file__).parents[1] / "data" / "jokes.json"
+
+
+with open(jokes_path, "r", encoding="utf-8") as file:
+    jokes = json.load(file)
+
+
+def main():
+    with app.get_producer() as producer:
+        # print(producer)
+        for joke in jokes:
+            kafka_msg = jokes_topic.serialize(key=joke["joke_id"], value=joke)
+
+            print(
+                f"produce event with key = {kafka_msg.key}, value = {kafka_msg.value}"
+            )
+            producer.produce(
+                topic=jokes_topic.name, key=str(kafka_msg.key), value= kafka_msg.value
+                )
+
+
+# pprint(jokes)
+
+if __name__ == "__main__":
+    main()
