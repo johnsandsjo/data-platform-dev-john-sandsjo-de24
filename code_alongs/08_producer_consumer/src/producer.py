1d1f4410e3245bbdf1eeb9aac697be48fdb540eb
John Sandsjö <john.sandsjo@gmail.com>
2025-01-27 11:33:56 +0100

    code along 08

---
+++ code_alongs/08_producer_consumer/src/producer.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+import json
+from pprint import pprint
+
+
+data_path = Path(__file__).parents[1] / "data"
+
+with open(data_path / "jokes.json", "r") as file:
+    jokes = json.load(file)
+
+pprint(jokes)
