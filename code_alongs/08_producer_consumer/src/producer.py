3f2681b627e158fd9608dade9efb51aefc1f936c
John Sandsjö <john.sandsjo@gmail.com>
2025-01-27 13:08:18 +0100

    code-along 08

--- code_alongs/08_producer_consumer/src/producer.py
+++ code_alongs/08_producer_consumer/src/producer.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import json
 from pprint import pprint
+from quixstreams import Application
 
 
 data_path = Path(__file__).parents[1] / "data"
@@ -8,4 +9,31 @@ data_path = Path(__file__).parents[1] / "data"
 with open(data_path / "jokes.json", "r") as file:
     jokes = json.load(file)
 
-pprint(jokes)
+# pprint(jokes)
+
+# app
+app = Application(broker_address="localhost:9092", consumer_group="text-splitter")
+
+# speca en kod
+jokes_topic = app.topic(name="jokes", value_serializer="json")
+
+# print(jokes_topic)
+
+
+def main():
+    with app.get_producer() as producer:
+        # print(producer)
+        for joke in jokes:
+            kafka_msg = jokes_topic.serialize(key=joke["joke_id"], value=joke)
+            print(f"Produced message: key = {kafka_msg.key}, value = {kafka_msg.value}")
+
+            # dags att producera
+            producer.produce(
+                topic="jokes", key=str(kafka_msg.key), value=kafka_msg.value
+            )
+
+
+# run this code only when executing this script and not when importing the module
+if __name__ == "__main__":
+    # pprint(jokes)
+    main()
