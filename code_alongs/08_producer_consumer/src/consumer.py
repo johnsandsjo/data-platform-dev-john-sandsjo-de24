59d024476634d78c9f07e348b095157a283f8bad
John Sandsjö <john.sandsjo@gmail.com>
2025-01-28 10:52:46 +0100

    code along 08

---
+++ code_alongs/08_producer_consumer/src/consumer.py
@@ -0,0 +1,28 @@
+from quixstreams import Application
+
+app = Application(broker_address= "localhost:9092", consumer_group= "text-splitter", auto_offset_reset="earliest")
+
+jokes_topic = app.topic(name="jokes", value_deserializer="json")
+
+#detta är inte en pandas dataframe, en annan typ av dataframe. Streaming data frame
+sdf = app.dataframe(topic=jokes_topic)
+
+sdf.update(lambda message: print(f"Input message: {message} \n"))
+
+# detta är sammamotsvarande för lamda funktionen ovan
+#def print_message(message):
+#   print(message)
+#self.update(print_message)
+
+#nu gör vi en transformation. Vill splita mellan orden (bara för test)
+def split_words(message):
+    return [{"word": word} for word in message["joke_text"].split()]
+
+sdf = sdf.apply(split_words, expand=True)
+
+sdf["length"] = sdf["word"].apply(lambda word: len(word))
+
+sdf.update(lambda row: print(f"Output: {row}"))
+
+if __name__ == '__main__':
+    app.run()
--- code_alongs/08_producer_consumer/src/producer.py
+++ code_alongs/08_producer_consumer/src/producer.py
@@ -6,20 +6,24 @@ from quixstreams import Application
 
 data_path = Path(__file__).parents[1] / "data"
 
+#reads in jokes.json in absolute path (but relative typ)
 with open(data_path / "jokes.json", "r") as file:
     jokes = json.load(file)
 
 # pprint(jokes)
 
-# app
+# här startar vi en applikation. Entry point för Kafka. 
+#9092 är den som vår container är mappad mot (ports mapped to broker container 9092) Så att man kan prata med broker
 app = Application(broker_address="localhost:9092", consumer_group="text-splitter")
 
-# speca en kod
+# skapadae en topic som vi kallade jokes. serialisation gör om json objektet till ett format som kan skickas i kommunikation.
 jokes_topic = app.topic(name="jokes", value_serializer="json")
 
 # print(jokes_topic)
 
-
+#här skapar vi vår prodcuer
+#loopar genom json array och serialiserar
+#producerar med .produce key och value
 def main():
     with app.get_producer() as producer:
         # print(producer)
